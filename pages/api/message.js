94b7061b12bb3bad62538d3faa07499c375abc4d
StealthWorm <thierrypitela@hotmail.com>
2023-04-15 13:18:32 -0300

    first commit 2

--- pages/api/message.js
+++ pages/api/message.js
@@ -2,14 +2,15 @@ export default function handler(req, res) {
   const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
   var messageResponse = new MessagingResponse();
-
+  
   messageResponse.message('Reply goes here');
+
   // send response
   res.writeHead(200, {
-    'Content-Type': 'text/xml'
+  'Content-Type': 'text/xml'
   });
   res.end(messageResponse.toString());
-}
+  }
 
 // import { Configuration, OpenAIApi } from "openai";
 
