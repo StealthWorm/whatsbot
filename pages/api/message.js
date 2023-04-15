26ac365b6c149d4b91737efe66f628fe95f0e5b5
StealthWorm <thierrypitela@hotmail.com>
2023-04-15 15:46:29 -0300

    first commit 17

--- pages/api/message.js
+++ pages/api/message.js
@@ -47,7 +47,7 @@ export default async function handler(req, res) {
 
     } catch (error) {
       if (error.response) {
-        console.log(error.response)
+        console.log(error)
         replyToBeSent = "There was an issue with the server"
       } else { // error getting response
         replyToBeSent = "An error occurred during your request.";
