c31abc9c6432123060db387bbb5281bce93c010f
StealthWorm <thierrypitela@hotmail.com>
2023-04-26 20:08:53 -0300

    new test 4

--- pages/api/message.js
+++ pages/api/message.js
@@ -68,8 +68,9 @@ export default async function handler(req, res) {
       'Content-Type': 'text/xml'
     });
 
-    // res.end(messageResponse.toString());
-    res.status(200).json({ text: `${messageResponse.toString()}` })
+    res.end(messageResponse.toString());
+    console.log("passou aqui")
+    // res.status(200).json({ text: `${messageResponse.toString()}` })
   } catch (error) {
     console.error('Error sending Twilio response:', error);
     res.status(500).json({
