4d44111e8fdf450ea533f5f0caea81c4e6fbcbd5
StealthWorm <thierrypitela@hotmail.com>
2023-04-26 19:36:00 -0300

    new test

--- pages/api/message.js
+++ pages/api/message.js
@@ -24,13 +24,12 @@ export default async function handler(req, res) {
   const MessagingResponse =  require('twilio').twiml.MessagingResponse;
   var messageResponse = new MessagingResponse();
 
-  const sentMessage = req.body.Body || '';
+  const sentMessage = req.body || ''; //req.body.Body 
   let replyToBeSent = "";
 
   if (sentMessage.trim().length === 0) {
     replyToBeSent = "We could not get your message. Please try again";
   } else {
-
     if (!configuration.apiKey) {
       res.status(500).json({
         error: {
@@ -51,13 +50,13 @@ export default async function handler(req, res) {
       });
 
       replyToBeSent = removeIncompleteText(completion.data.choices[0].text)
-      console.log(replyToBeSent)
+      // console.log(replyToBeSent)
 
     } catch (error) {
       if (error.response) {
         console.log(error.response)
         replyToBeSent = JSON.stringify(error.response)
-        replyToBeSent = "There was an issue with the server"
+        // replyToBeSent = "There was an issue with the server"
       } else { 
         replyToBeSent = "An error occurred during your request.";
       }
@@ -72,7 +71,7 @@ export default async function handler(req, res) {
     'Content-Type': 'text/xml'
   });
 
-  res.end(messageResponse.toString());
+  res.send(messageResponse.toString());
 }
 
 function removeIncompleteText(inputString) {
